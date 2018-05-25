e535753d5d010aebf14b736c244f3177c60148a6
Amandine CESAR <amandine.cesar@campus-numerique-in-the-alps.com>
2018-05-25 17:19:08 +0200

    new Bundle

---
+++ src/ProjectBundle/Controller/ProjectController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ProjectBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * @Route("/", name="project")
+ */
+class ProjectController extends Controller
+{
+}
